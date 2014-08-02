383c207fd37ef8cc71c2d24ec01dd7f5a9a2e580
Ganeshcoep <ganeshsalvi.coep@gmail.com>
2014-08-02 02:53:07 -0500

    Added UsefulToKnow.java
    Useful things/functions to know for java coding.

---
+++ src/UsefulToKnow.java
@@ -0,0 +1,47 @@
+/*
+ * Useful things/functions to know for java coding 
+ * 
+ */
+import java.util.Arrays;
+public class UsefulToKnow {
+
+	public UsefulToKnow() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String args[]){		
+/*		
+		toCharArray() //get char array of a String
+		Arrays.sort()  //sort an array
+		Arrays.toString(char[] a) //convert to string
+		charAt(int x) //get a char at the specific index
+		length() //string length
+		length //array size 
+		substring(int beginIndex) 
+		substring(int beginIndex, int endIndex)
+		Integer.valueOf()//string to integer
+		String.valueOf()/integer to string
+		Can int(primitive data type) array be null in Java 
+*/
+		UsefulToKnow p= new UsefulToKnow();		
+		String test="testString";
+		char[] myArray=test.toCharArray();
+		System.out.println(myArray[5]);
+		int [] a={11,4,5,2};
+		Arrays.sort(a);
+		System.out.println(a[0]);		
+		String myString=Arrays.toString(myArray);
+		System.out.println(myString+test);
+		System.out.println(test.charAt(1));
+		System.out.println(test.length());
+		System.out.println(myArray.length);
+		System.out.println(test.substring(1));
+		System.out.println(test.substring(1,4));
+		System.out.println(Integer.valueOf("123"));
+		System.out.println(String.valueOf(1234));
+		System.out.println(Arrays.binarySearch(a,11));		
+		// Can int(primitive data type) array be null in Java 
+		int[] A=null; // yes	
+		
+	}
+}
