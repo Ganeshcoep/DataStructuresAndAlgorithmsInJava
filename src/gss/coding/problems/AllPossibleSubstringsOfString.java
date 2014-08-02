dfe0be050ceb0be431e1cce662b841f114f62c5d
Ganeshcoep <ganeshsalvi.coep@gmail.com>
2014-08-02 02:25:23 -0500

    Added solution for allPossibleSubstrings
    Program to print total Number of all possible substrings of the given
    string

---
+++ src/gss/coding/problems/AllPossibleSubstringsOfString.java
@@ -0,0 +1,40 @@
+/*Program to print total Number of all possible substrings of the given string
+ *Substring of string is formed by removing one or more characters from the left or right of the given string
+ * 
+ * Note: String winwin has duplicate substrings
+ * 
+ */
+package gss.coding.problems;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class AllPossibleSubstringsOfString {
+	public static void main(String args[]){
+		String input="kin";
+		//Initialize hashmap here since there might be duplicate substrings starting at any index of String
+		HashMap<String,String> hmap=new HashMap<String, String>();
+		for(int len=1;len<input.length();len++){
+			for(int startIndex=0;startIndex<input.length();startIndex++){
+				String temp = null;
+				if((startIndex+len-1)<input.length()){
+					temp=input.substring(startIndex, startIndex+len);
+					hmap.put(temp, temp);		    	
+				}
+			}
+		}
+		System.out.println("Total no. of substrings : "+hmap.size());
+		// print all the substrings
+		System.out.println("All substrings: ");
+		for (Iterator<Entry<String, String>> it =hmap.entrySet().iterator(); it.hasNext();) {
+			Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
+			System.out.println(entry.getValue());
+
+		}
+	}
+}
+
+
+
