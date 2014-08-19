1896eb0b07306c7e4b1c7b7f6b8c56ad1c303349
Ganeshcoep <ganeshsalvi.coep@gmail.com>
2014-08-18 21:39:54 -0500

    Added solution for majority element problem
    Added solution for majority element problem

---
+++ src/codility/leader/Leader.java
@@ -0,0 +1,48 @@
+package codility.leader;
+
+public class Leader {
+	/**
+	 * @param args
+	 */	
+	public static int getCandidate(int A[]){		
+		int count=0;
+		int candidate=-1;
+		for (int i = 0; i < A.length; i++) {
+			if(count==0){
+				count+=1;
+				candidate=A[i];
+			}else{
+				if(A[i]==candidate){
+					count++;
+				}
+				else
+					count--;			
+			}
+		}		
+		return candidate;		
+	}
+	
+	public static boolean checkMajority(int candidate,int A[]){
+		int count=0;		
+		for (int i = 0; i < A.length; i++) {
+			if(A[i]==candidate){
+				count++;
+			}					
+		}
+		
+		if(count>A.length/2){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A[]={1,2,1,2,5,3,2,2};
+		System.out.println(getCandidate(A)); // gives possible majority candidate
+		System.out.println(checkMajority(getCandidate(A),A));//returns true if majority element is present
+
+	}
+
+}
