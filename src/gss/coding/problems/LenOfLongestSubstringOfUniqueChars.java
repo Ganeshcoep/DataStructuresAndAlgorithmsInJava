8bfac6a4e10ec588846e5c6c3e235fc190cc2ffb
Ganeshcoep <ganeshsalvi.coep@gmail.com>
2015-08-11 00:06:05 -0700

    Added solution for problem LenOfLongestSubstringOfUniqueChars

--- src/gss/coding/problems/LenOfLongestSubstringOfUniqueChars.java
+++ src/gss/coding/problems/LenOfLongestSubstringOfUniqueChars.java
@@ -1,6 +1,12 @@
-/* Find the lenght of the longest substring of the unique or non repeated chars
+/* Find the length of the longest substring of the unique or non repeated chars
  */
 package gss.coding.problems;
+
+/*
+ * Few other references:
+ * http://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+ * http://www.programcreek.com/2013/02/leetcode-longest-substring-without-repeating-characters-java/
+ */
 
 
 public class LenOfLongestSubstringOfUniqueChars {
@@ -13,11 +19,12 @@ public class LenOfLongestSubstringOfUniqueChars {
 		char[] s=str.toCharArray();
 		//int i=0;
 		int startIndex=1;
-		int[] chars=new int[26];
+		int[] chars=new int[26]; // assuming char set is 26
 		int maxLen=0;
 		//Initialize it to zero;
 		for(int k=0;k<26;k++)
 			chars[k]=0;		
+		
 		for(int i=0;i<len;i++){
 			int intValue=s[i]-'a';
 			if(chars[intValue]!=0){
@@ -25,9 +32,9 @@ public class LenOfLongestSubstringOfUniqueChars {
 				maxLen=max(maxLen,tempMaxLen);
 				startIndex=chars[intValue]+1;
 			}
-				chars[intValue]=i+1;
-			
+			chars[intValue]=i+1;			
 		}
+		
 		int lastMax=len-startIndex+1;		
 		maxLen=max(maxLen,lastMax);
 		return maxLen;
@@ -39,7 +46,7 @@ public class LenOfLongestSubstringOfUniqueChars {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str="aaabcdeaa";
+		String str="abcdaebh";//aaabcdeaa";
 		System.out.println(lengthOfLongestSubstringOfUniqueChars(str));
 
 	}
