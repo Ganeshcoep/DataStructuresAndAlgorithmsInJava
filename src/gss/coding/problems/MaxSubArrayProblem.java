1e7f0c433f53669d0d361fea7b8d49c6d487fbcc
Ganeshcoep <ganeshsalvi.coep@gmail.com>
2015-08-16 17:37:36 -0700

    Added solution for max sub array problem with O(1) and O(n) space/ time complexity respectively

--- src/gss/coding/problems/MaxSubArrayProblem.java
+++ src/gss/coding/problems/MaxSubArrayProblem.java
@@ -34,7 +34,8 @@ public class MaxSubArrayProblem {
 		 */
 		int[] maxSumAmongSubarraysEndingAtIndex = new int [input.length];
 		
-		maxSumAmongSubarraysEndingAtIndex[0] = input[0];		
+		maxSumAmongSubarraysEndingAtIndex[0] = input[0];
+		
 		for(int i = 1 ; i < input.length ; ++i )
 		{
 			if(maxSumAmongSubarraysEndingAtIndex[i-1] + input[i] > input[i]){
@@ -55,24 +56,57 @@ public class MaxSubArrayProblem {
 		return maxSum;		
 	}
 
+	/*
+	 *  This function is copy of above function, except that it avoids using the 
+	 *  extra space
+	 *  
+	 *  Time complexity 	: O(n)
+	 *  Space complexity 	: O(n)
+	 */
+	
+	public static int maxSumWithReducedSpaceComplexity(int[] input){		
+		int maxSumSoFar = input[0] ;
+		int maxSumEndingAtPreviousIndex = input[0]; 		
+		for(int i = 1 ; i < input.length ; ++i )
+		{
+			int maxSumAtCurrentindex = 0;
+			if(maxSumEndingAtPreviousIndex + input[i] > input[i]){
+				maxSumAtCurrentindex = maxSumEndingAtPreviousIndex + input[i]; 
+			}else{
+				maxSumAtCurrentindex = input[i];
+			}			
+			maxSumEndingAtPreviousIndex = maxSumAtCurrentindex;
+			if(maxSumAtCurrentindex > maxSumSoFar)
+				maxSumSoFar = maxSumAtCurrentindex;
+		}
+		return maxSumSoFar;
+	}
+
 	public static void main(String[] args) {
 		//Test 1
-		int arr0[] = {-2,11,-4,13,-5,2}; // answer is 20 from index 1 to 3
+		int arr0[] = {-2,11,-4,13,-5,2}; 				// answer is 20 from index 1 to 3
 		System.out.println(maxSum(arr0));
+		System.out.println(maxSumWithReducedSpaceComplexity(arr0));
+		
 		//Test 2
-		int arr1[] = {1,-3,4,-2,-1,6}; // answer is 7 from index 2 to 5
+		int arr1[] = {1,-3,4,-2,-1,6}; 					// answer is 7 from index 2 to 5
 		System.out.println(maxSum(arr1));		
+		System.out.println(maxSumWithReducedSpaceComplexity(arr1));
+		
 		//Test 3
-		int[] arr2 = {-2, -3, 4, -1, -2, 1, 5, -3};
-		System.out.println(maxSum(arr2)); //answer is 7 -from index 2 to 6
+		int[] arr2 = {-2, -3, 4, -1, -2, 1, 5, -3};		//answer is 7 -from index 2 to 6
+		System.out.println(maxSum(arr2)); 				
+		System.out.println(maxSumWithReducedSpaceComplexity(arr2));
 		
 		//Test 4 - all positive elements
-		int[] arr3 = {1,2,3,4,5};
-		System.out.println(maxSum(arr3)); //answer is 15 -from index 0 to 4
+		int[] arr3 = {1,2,3,4,5};						//answer is 15 -from index 0 to 4
+		System.out.println(maxSum(arr3)); 
+		System.out.println(maxSumWithReducedSpaceComplexity(arr3));
 		
 		//Test 5 - all -Ve elements
-		int[] arr4 = {-1,-2,-3,-4,-5};
-		System.out.println(maxSum(arr4)); //answer is -1 -from index 0 to 0
+		int[] arr4 = {-1,-2,-3,-4,-5};					//answer is -1 -from index 0 to 0
+		System.out.println(maxSum(arr4)); 
+		System.out.println(maxSumWithReducedSpaceComplexity(arr4));
 	}
 }
 
