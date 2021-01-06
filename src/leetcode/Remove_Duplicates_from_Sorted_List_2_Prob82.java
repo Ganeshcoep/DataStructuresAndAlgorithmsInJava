96d1711f129cc3681afa748a17b3fe05a5752cd2
Ganesh <ganeshsalvi.coep@gmail.com>
2021-01-05 21:50:24 -0800

    Add solution for LC82-Remove duplicates from LinkedList II

---
+++ src/leetcode/Remove_Duplicates_from_Sorted_List_2_Prob82.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import leetcodeDataStructuresUtil.ListNode;
+
+public class Remove_Duplicates_from_Sorted_List_2_Prob82 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public ListNode deleteDuplicates(ListNode head) {
+	    
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode sentinel = new ListNode(-1, head);
+        
+        ListNode last = sentinel;
+        
+        ListNode prev = head;
+        ListNode curr = head.next;
+        
+        while(curr != null)
+        {   
+            if(prev.val != curr.val){
+                last.next = prev;  
+                last = prev;
+            }
+
+            while(curr != null && prev.val == curr.val){
+                prev = curr;
+                curr = curr.next;                            
+            }
+
+            prev = curr;
+            
+            if(curr != null) curr = curr.next;
+        }
+
+        last.next = prev;
+        return sentinel.next;        
+    }
+}
