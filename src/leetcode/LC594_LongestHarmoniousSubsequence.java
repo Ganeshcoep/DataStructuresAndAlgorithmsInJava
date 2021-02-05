14bc89d1dfcf6a85717e4eb72c096190b8e12184
Ganesh <ganeshsalvi.coep@gmail.com>
2021-02-04 22:30:10 -0800

    Add solution for LC594_LongestHarmoniousSubsequence

---
+++ src/leetcode/LC594_LongestHarmoniousSubsequence.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.*;
+
+public class LC594_LongestHarmoniousSubsequence {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public class Solution {
+	    public int findLHS(int[] nums) {
+	        HashMap < Integer, Integer > map = new HashMap < > ();
+	        int res = 0;
+	        for (int num: nums) {
+	            map.put(num, map.getOrDefault(num, 0) + 1);
+	            if (map.containsKey(num + 1))
+	                res = Math.max(res, map.get(num) + map.get(num + 1));
+	            if (map.containsKey(num - 1))
+	                res = Math.max(res, map.get(num) + map.get(num - 1));
+	        }
+	        return res;
+	    }
+	}
+
+}
